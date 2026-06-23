54766d200f1a8257c6aea9bcc5fe81a29edd2568
DEV-trongphuc <DEV-trongphuc@users.noreply.github.com>
2026-06-23 14:15:01 +0700

    auto: update cache buster to v1782198901 and deploy

--- wp-content/themes/LANDINGPAGE_MBA/header.php
+++ wp-content/themes/LANDINGPAGE_MBA/header.php
@@ -24,7 +24,7 @@
   <?php endif; ?>
 
   <!-- Load JS sớm nhất -->
-  <script src="/wp-content/new_public/LANDINGPAGE_MBA/variable.js?v=1782198523"></script>
+  <script src="/wp-content/new_public/LANDINGPAGE_MBA/variable.js?v=1782198901"></script>
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
