373da7b40dd0ee6e7fc92a95b0fafb8dae2d0ea1
DEV-trongphuc <DEV-trongphuc@users.noreply.github.com>
2026-06-24 14:06:23 +0700

    auto: update cache buster to v1782284783 and deploy

--- inspect_public_html.php
+++ inspect_public_html.php
@@ -10,51 +10,24 @@ if (!file_exists($wp_load_path)) {
 
 require_once $wp_load_path;
 
-echo "=== wp_footer Hooks ===\n";
-global $wp_filter;
-if (isset($wp_filter['wp_footer'])) {
-    foreach ($wp_filter['wp_footer']->callbacks as $priority => $callbacks) {
-        echo "Priority: $priority\n";
-        foreach ($callbacks as $idx => $cb) {
-            echo "  Callback: ";
-            $function = $cb['function'];
-            if (is_string($function)) {
-                echo "$function\n";
-            } elseif (is_array($function)) {
-                if (is_object($function[0])) {
-                    echo get_class($function[0]) . "->" . $function[1] . "\n";
-                } else {
-                    echo $function[0] . "::" . $function[1] . "\n";
-                }
-            } else {
-                echo "[Closure/Object]\n";
-            }
+echo "=== Reflection of gej_eaz6koqqfg7x ===\n";
+if (function_exists('gej_eaz6koqqfg7x')) {
+    try {
+        $ref = new ReflectionFunction('gej_eaz6koqqfg7x');
+        echo "Function: gej_eaz6koqqfg7x\n";
+        echo "File: " . $ref->getFileName() . "\n";
+        echo "Line: " . $ref->getStartLine() . "\n";
+        
+        echo "\n=== Content around the function ===\n";
+        $file_lines = file($ref->getFileName());
+        $start_line = max(0, $ref->getStartLine() - 10);
+        $end_line = min(count($file_lines), $ref->getStartLine() + 30);
+        for ($i = $start_line; $i < $end_line; $i++) {
+            echo ($i + 1) . ": " . $file_lines[$i];
         }
+    } catch (Exception $e) {
+        echo "Reflection error: " . $e->getMessage() . "\n";
     }
 } else {
-    echo "No wp_footer hooks found.\n";
-}
-
-echo "\n=== login_footer Hooks ===\n";
-if (isset($wp_filter['login_footer'])) {
-    foreach ($wp_filter['login_footer']->callbacks as $priority => $callbacks) {
-        echo "Priority: $priority\n";
-        foreach ($callbacks as $idx => $cb) {
-            echo "  Callback: ";
-            $function = $cb['function'];
-            if (is_string($function)) {
-                echo "$function\n";
-            } elseif (is_array($function)) {
-                if (is_object($function[0])) {
-                    echo get_class($function[0]) . "->" . $function[1] . "\n";
-                } else {
-                    echo $function[0] . "::" . $function[1] . "\n";
-                }
-            } else {
-                echo "[Closure/Object]\n";
-            }
-        }
-    }
-} else {
-    echo "No login_footer hooks found.\n";
+    echo "Function gej_eaz6koqqfg7x does not exist in the loaded environment.\n";
 }
