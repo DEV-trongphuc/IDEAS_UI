b2a06df2cf24d6c8dc2a7c010fd22d8c13558217
DEV-trongphuc <DEV-trongphuc@users.noreply.github.com>
2026-07-08 15:52:41 +0700

    temp: run global check of program.js and data_imgs

--- run_search_temp_99.php
+++ run_search_temp_99.php
@@ -1,23 +1,53 @@
 <?php
 header('Content-Type: text/plain; charset=utf-8');
 
-$dir = '/home/vhvxoigh/ideas.edu.vn/wp-content/new_public';
+$search_roots = [
+    'ideas.edu.vn' => '/home/vhvxoigh/ideas.edu.vn',
+    'public_html' => '/home/vhvxoigh/public_html',
+    'workshop.chief' => '/home/vhvxoigh/workshop.chiefaiofficer.vn',
+    'cpanel_home' => '/home/vhvxoigh'
+];
+
 $results = [];
 
-if (is_dir($dir)) {
-    $iterator = new RecursiveIteratorIterator(
-        new RecursiveDirectoryIterator($dir, FilesystemIterator::SKIP_DOTS),
-        RecursiveIteratorIterator::SELF_FIRST
-    );
-    
-    foreach ($iterator as $file) {
-        if ($file->isFile()) {
-            $rel = str_replace('/home/vhvxoigh/ideas.edu.vn/', '', $file->getPathname());
-            $results[] = [
-                'path' => $rel,
-                'size' => $file->getSize()
-            ];
-        }
+foreach ($search_roots as $label => $sdir) {
+    if (is_dir($sdir)) {
+        try {
+            // Only scan shallow directories in cpanel_home to avoid infinite recursion or heavy load,
+            // but run a full search in the site dirs.
+            $max_depth = ($label === 'cpanel_home') ? 2 : 8;
+            
+            $dir_iter = new RecursiveDirectoryIterator($sdir, FilesystemIterator::SKIP_DOTS);
+            $iterator = new RecursiveIteratorIterator($dir_iter, RecursiveIteratorIterator::SELF_FIRST);
+            $iterator->setMaxDepth($max_depth);
+            
+            try {
+                $iterator->rewind();
+            } catch (Exception $e) {}
+            
+            while ($iterator->valid()) {
+                try {
+                    $file = $iterator->current();
+                    $name = $file->getFilename();
+                    $lower_name = strtolower($name);
+                    
+                    if ($lower_name === 'program.js' || $lower_name === 'data_imgs') {
+                        $size = $file->isDir() ? 0 : $file->getSize();
+                        $rel_path = str_replace('/home/vhvxoigh/', '', $file->getPathname());
+                        $results[] = [
+                            'site' => $label,
+                            'path' => $rel_path,
+                            'size' => $size,
+                            'is_dir' => $file->isDir()
+                        ];
+                    }
+                } catch (Exception $e) {}
+                
+                try {
+                    $iterator->next();
+                } catch (Exception $e) {}
+            }
+        } catch (Exception $e) {}
     }
 }
 
