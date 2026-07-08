cd14d6d039654f00b116e877ad299e48e9bf0aee
DEV-trongphuc <DEV-trongphuc@users.noreply.github.com>
2026-07-08 15:51:59 +0700

    temp: run list of new_public files

--- run_search_temp_99.php
+++ run_search_temp_99.php
@@ -1,56 +1,23 @@
 <?php
 header('Content-Type: text/plain; charset=utf-8');
 
-$search_dirs = [
-    'ideas.edu.vn' => '/home/vhvxoigh/ideas.edu.vn',
-    'public_html' => '/home/vhvxoigh/public_html',
-    'workshop.chief' => '/home/vhvxoigh/workshop.chiefaiofficer.vn'
-];
-
+$dir = '/home/vhvxoigh/ideas.edu.vn/wp-content/new_public';
 $results = [];
-$keywords = ['icon1', 'icon2', 'icon3', 'icon4', 'ai.webp', 'data_imgs'];
 
-foreach ($search_dirs as $label => $sdir) {
-    if (is_dir($sdir)) {
-        try {
-            $dir_iter = new RecursiveDirectoryIterator($sdir, FilesystemIterator::SKIP_DOTS);
-            $iterator = new RecursiveIteratorIterator($dir_iter, RecursiveIteratorIterator::SELF_FIRST);
-            
-            try {
-                $iterator->rewind();
-            } catch (Exception $e) {}
-            
-            while ($iterator->valid()) {
-                try {
-                    $file = $iterator->current();
-                    $name = $file->getFilename();
-                    $lower_name = strtolower($name);
-                    
-                    $matched = false;
-                    foreach ($keywords as $kw) {
-                        if (strpos($lower_name, $kw) !== false) {
-                            $matched = true;
-                            break;
-                        }
-                    }
-                    
-                    if ($matched) {
-                        $size = $file->isDir() ? 0 : $file->getSize();
-                        $rel_path = str_replace('/home/vhvxoigh/', '', $file->getPathname());
-                        $results[] = [
-                            'site' => $label,
-                            'path' => $rel_path,
-                            'size' => $size,
-                            'is_dir' => $file->isDir()
-                        ];
-                    }
-                } catch (Exception $e) {}
-                
-                try {
-                    $iterator->next();
-                } catch (Exception $e) {}
-            }
-        } catch (Exception $e) {}
+if (is_dir($dir)) {
+    $iterator = new RecursiveIteratorIterator(
+        new RecursiveDirectoryIterator($dir, FilesystemIterator::SKIP_DOTS),
+        RecursiveIteratorIterator::SELF_FIRST
+    );
+    
+    foreach ($iterator as $file) {
+        if ($file->isFile()) {
+            $rel = str_replace('/home/vhvxoigh/ideas.edu.vn/', '', $file->getPathname());
+            $results[] = [
+                'path' => $rel,
+                'size' => $file->getSize()
+            ];
+        }
     }
 }
 
